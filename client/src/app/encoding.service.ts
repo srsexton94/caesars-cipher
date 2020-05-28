ed8fce3586977d358a46f54655ff73e4e3f87060
srsexton94 <srsexton94@gmail.com>
2020-05-28 14:56:27 -0400

    Add function to point client to production API
    Add function `configUrl` to the client EncodingService so that, when in
    development it will point to the development server - and when in
    production it will point to the deployed server
    (Note, server not yet deployed, placeholder used)

--- client/src/app/encoding.service.ts
+++ client/src/app/encoding.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Cipher } from './cipher';
 
+const configUrl = () => {
+  if (window.location.hostname === 'localhost') {
+    return 'http://localhost:3000/ciphers'
+  } else {
+    return '<replace-with-production-url>'
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class EncodingService {
-  _url = 'http://localhost:3000/ciphers'
+  _url = configUrl()
   constructor(
     private _http: HttpClient
   ) { }
