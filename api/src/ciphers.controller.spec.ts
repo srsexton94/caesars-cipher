bb3aa448888b07d6f2d0462b2e227ac7f16f0c19
srsexton94 <srsexton94@gmail.com>
2020-05-28 14:28:00 -0400

    Rewrite Ciphers controller spec, successful test
    Rewrite the controller spec for the Ciphers resource 'POST' request,
    `addCipher()` -- passes jest test

--- api/src/ciphers.controller.spec.ts
+++ api/src/ciphers.controller.spec.ts
@@ -4,7 +4,7 @@ import { CiphersService } from './ciphers.service';
 
 // 'describe' groups similar unit test cases
 describe('CiphersController', () => {
-  let ciphersController: CiphersController;
+  let ciphersController: CiphersController
 
   // runs this code before each test case (also beforeAll, afterEach, afterAll)
   beforeEach(async () => {
@@ -12,25 +12,27 @@ describe('CiphersController', () => {
     const ciphers: TestingModule = await Test.createTestingModule({
       controllers: [CiphersController],
       providers: [CiphersService],
-    }).compile();
+    }).compile()
 
     ciphersController = ciphers.get<CiphersController>(CiphersController);
-  });
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
 
   describe('addCipher', () => {
     // 'it' --> independent unit  test case, may have multiple per 'describe'
     it('should return the encoded string', async () => {
+
       // mock up the HTTP 'Post' request
-      const result = AxiosResponse = {
-        data: {
-            encoded: 'E Hkra FwrwOynelp!'
-        }
+      const expectedResult = {
+        encoded: 'E Hkra FwrwOynelp!'
       }
-      // 'spyOn' mocks the function & forces return
-      // 'expect' validates the output, may have multiple per test
-      expect(jest.spyOn(ciphersController, 'addCipher').mockImplementationOnce((url: string, config: AxiosRequestConfig) => result)
-        .toEqual(result)
-      // expect(await ciphersController.addCipher()).toBe(result)
-    });
-  });
-});
+
+      const result = await ciphersController.addCipher('I Love JavaScript!', 100)
+
+      expect(result).toEqual(expectedResult)
+    })
+  })
+})
